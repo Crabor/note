df889551775916a451e9c6d31c9789a363afe0b3
Crabor <crabor19711211@163.com>
2019-12-11 22:37:39 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
-#include "CPP.h"
 #include <iostream>
 
 using namespace std;
 
-class test {
+class class1 {
+    int x;
+    int y;
+    virtual int add() { return x + y; };
+    void setX(int x) { this->x = x; }
 };
-
-// test::test(/* args */)
-// {
-// }
-
-// test::~test()
-// {
-// }
-
-// void test::func() {}
-
+struct struct1 {
+    int x;
+    int y;
+    virtual int add() { return x + y; };
+    void setX(int x) { this->x = x; }
+};
+class class2 : class1 {
+};
+struct struct2 : struct1 {
+};
 int main(void)
 {
-    cout << sizeof(test) << endl;
+    cout << sizeof(class1) << endl;
+    cout << sizeof(class2) << endl;
+    cout << sizeof(struct1) << endl;
+    cout << sizeof(struct2) << endl;
     return 0;
 }
